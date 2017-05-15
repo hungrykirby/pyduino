2a2ea217601dd3f20057dc1ac135fb8a30f15a74
Tutanrhamun <katsuki.hanawa.job@gmail.com>
2017-05-15 12:43:05 +0900

    aaa

--- app.py
+++ app.py
@@ -1,24 +1,47 @@
 import serial
 import re
+import threading
+import sys
+
 import arrange
 
-def main():
+c = "0"
+
+def serial_loop():
     with serial.Serial('COM3',9600,timeout=0.1) as ser:
         arra = arrange.Arrange(ser)
         try:
             while True:
-                c = ser.readline()
-                de = c.decode('utf-8')
+                s = ser.readline()
+                de = s.decode('utf-8')
                 m = re.match("\-*[\w]+", str(de))
                 if(m != None):
                     #print(m.group())
-                    arra.fetch_three_numbers(m.group())
+                    arra.fetch_three_numbers(m.group(), c)
                 else:
                     pass
                     #print(type(m))
-        except KeyboardInterrupt:
-            ser.close()
+        except:
+             print("Unexpected error:", sys.exc_info()[0])
+             raise
         ser.close()
+
+ser_loop = threading.Thread(target=serial_loop,name="ser_loop",args=())
+ser_loop.setDaemon(True)
+ser_loop.start()
+
+def main():
+    global c
+    while True:
+        tmp = input()
+        if c != tmp:
+            c = tmp
+            print("c =", c)
+        else:
+            pass
+        if tmp == 'a':
+            sys.exit()
+
 
 
 if __name__ == "__main__":
--- arrange.py
+++ arrange.py
@@ -1,12 +1,17 @@
 import numpy as np
 from scipy import signal
 from pylab import *
+import os
 
 class Arrange:
+    past_c = "xx"
     bwn_a = False
     ax = None
     n = np.arange(-0.5, 0.5, 0.01)
     plots_numbers = []
+    MODE = "train"
+    count = 0
+    pattern = {"train": 30, "test":10}
     def __init__(self, serial):
         self.ser = serial
 
@@ -21,7 +26,19 @@ class Arrange:
         plot(z, 5, 'd')
         pause(0.00001)
 
-    def fetch_three_numbers(self, matched_group):
+    def write_ceps(self, ceps, fn_):
+        fn = os.path.join(os.getcwd(), self.MODE, fn_)
+        if not os.path.exists(fn):
+            os.makedirs(fn)
+        #base_fn,ext = os.path.splitext(fn)
+        data_fn = os.path.join(self.MODE, fn_, str(self.count) + ".ceps")
+        np.save(data_fn,ceps)
+        self.count += 1
+
+    def fetch_three_numbers(self, matched_group, c):
+        if self.past_c != c:
+            self.past_c = c
+            self.count = 0
         try:
             if matched_group == "a":
                 self.bwn_a = True
@@ -34,6 +51,10 @@ class Arrange:
                 #self.plots(pl[0], pl[1], pl[2])
                 self.bwn_a = False
                 self.ser.flushInput()
-                print(pl)
+                print(c, pl)
+                if self.count < self.pattern[self.MODE]:
+                    self.write_ceps(np.array(pl).np.array(pl), c)
+                    print(pl)
+                    print(np.array(pl).astype(np.int64))
         except KeyboardInterrupt:
             ser.close()
